acd55888379d6d2760ea2d75ac36feae6ff078b5
Nouhaila El Brighli <nouhailaelbrighli1337@gmail.com>
2026-09-24 13:15:12 +0100

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type = 10
+float_type = 19.12
+bool_type = True
+string_type = "nouhaila"
+list_type = [1, 2, 4]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
