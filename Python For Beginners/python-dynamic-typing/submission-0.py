bc7f056835ebc0dd8c117046c5fc6f266ae24a54
Nouhaila El Brighli <nouhailaelbrighli1337@gmail.com>
2026-09-25 15:29:57 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,19 @@
+variable = 0;
+
+print(type(variable))
+
+variable = 1.4;
+
+print(type(variable))
+
+variable = False;
+
+print(type(variable))
+
+variable = "NOUHA"
+
+print(type(variable))
+
+variable = [1,3, 5]
+
+print(type(variable))
